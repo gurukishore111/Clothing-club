11d828cd570899d69b4711a7a5e142999278b43e
Guru kishore <52570524+gurukishore111@users.noreply.github.com>
2020-12-05 10:15:22 +0530

    Update our menu-items to navigate use withRouter Hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Switch,Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.components';
+
+const Hatspage = () =>{
+  return <div>
+    <h1>HATS PAGE</h1>
+  </div>
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>   
+        <Route exact path="/" component={HomePage}/>
+        <Route exact path="/hats" component={Hatspage}/>
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size ,match,linkUrl ,history }) =>{
+  return(
+  <div className={`${size} menu-item`} onClick={() =>history.push(`${match.url}${linkUrl}`)}>
     <div
       className='background-image'
       style={{
@@ -15,6 +16,6 @@ const MenuItem = ({ title, imageUrl, size }) => (
       <span className='subtitle'>SHOP NOW</span>
     </div>
   </div>
-);
+)}
 
-export default MenuItem;
+export default withRouter(MenuItem); 
